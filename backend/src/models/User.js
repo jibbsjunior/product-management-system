375b49150278702efd56b72b5cdd77fd076ea5ff
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-09 06:58:02 -0800

    setup a save method on user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
 
@@ -52,5 +53,15 @@ const UserSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+UserSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+        await bcrypt
+            .hash(user.password, 10)
+            .then((hash) => user.password = hash)
+            .catch((ex) => console.log(ex));
+    }
+})
 
 module.exports = mongoose.model("User", UserSchema);
