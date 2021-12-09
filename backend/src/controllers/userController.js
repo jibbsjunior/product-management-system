61145b429a9c71e9a9e562f2dd7147d7231dc195
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-09 08:29:13 -0800

    setting up geoCode with Goodle Place API

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,15 +1,25 @@
 const User = require('../models/User');
+const axios = require('axios');
 
+const signup = async (req, res, next) => {
+    try {
+        
+        const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=lagos&key=${process.env.GOOGLE_API_KEY}`)
+        res.json(data);
+        
+    } catch (ex) {
+        next(ex);
+    }
 
-const signup = (req, res) => {
-    User.findOne({ email: req.body.email }, async (err, user) => {
-        if (err) return res.status(400).json({ msg: "whooops! user already exist" });
+    // User.findOne({ email: req.body.email }, async (err, user) => {
+    //     if (err) return res.status(400).json({ msg: "whooops! user already exist" });
 
-        const newUser = new User({
-            name: req.body.name,
-            email: req.body.email
-        });
-    })
+    //     const newUser = new User({
+    //         name: req.body.name,
+    //         email: req.body.email,
+    //         password: req.body.password
+    //     });
+    // })
 }
 
 const login = (req, res) => {
